5dfb385d9280224e82d8fe32e7d2473b1849c170
silassentinel <53788992+Silassentinel@users.noreply.github.com>
2021-08-11 15:09:30 +0200

    unverified

--- copilotTestsJs.js
+++ copilotTestsJs.js
@@ -1,15 +1,15 @@
 // calculate average of 2 numbers
-var average = function(a, b) {
+const average = function(a, b) {
     return (a + b) / 2;
 }
 
 // readfile and parse to JSON
-var readFile = function(fileName) {
+const readFile = function(fileName) {
     return JSON.parse(fs.readFileSync(fileName, 'utf8'));
 }
 
 //readfile async show on console
-var readFileAsync = function(fileName) {
+const readFileAsync = function(fileName) {
     return new Promise(function(resolve, reject) {
         fs.readFile(fileName, 'utf8', function(err, data) {
             if (err) {
@@ -22,6 +22,41 @@ var readFileAsync = function(fileName) {
 }
 
 // take list of names and sort alphabetic
-var sortNames = function(names) {
+const sortNames = function(names) {
     return names.sort();
+}
+
+//show Fibonacci sequence on console
+const showFibonacci = (amount) => 
+{
+    // take input from the user
+    const number = parseInt(amount);
+    let n1 = 0, n2 = 1, nextTerm;
+
+    console.log('Fibonacci Series:');
+
+    for (let i = 1; i <= number; i++) 
+    {
+        console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+}
+
+// fibonacci sequence
+const printFibonacci = (number) => 
+{
+    let n = 0;
+    let n1 = 1;
+    let n2;
+    console.log(n);
+    console.log(n1);
+    while (n2 < number)
+    {
+        n2 = n+n1;
+        n=n1;
+        n1 = n2;
+        console.log(n2);
+    }
 }
